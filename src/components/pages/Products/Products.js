54d12f1b5029448109970fe03102716c8ed20cf6
Karthik Nishanth <karthik.nishanth06@gmail.com>
2021-05-11 15:25:36 +0530

    'lavish'

--- src/components/pages/Products/Products.js
+++ src/components/pages/Products/Products.js
@@ -1,13 +1,12 @@
-import React from 'react'
-import HeroSection from "../../HeroSection"
-import Pricing from '../../Pricing'
-import { homeObjFour } from './Data'
+import React from "react";
+import HeroSection from "../../HeroSection";
+import { homeObjFour } from "./Data";
 function Products() {
-    return (
-        <>
-            <HeroSection {...homeObjFour} />
-        </>
-    )
+  return (
+    <>
+      <HeroSection {...homeObjFour} />
+    </>
+  );
 }
 
-export default Products
+export default Products;
